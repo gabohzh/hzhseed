62043c0a61f39f3c1815bb4a72e86856bd116e34
Gabriel Avalos <gabriel_hzh@hotmail.com>
2023-05-06 17:27:38 -0500

    update

---
+++ src/config/exportSeed.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Seed output path
+    |--------------------------------------------------------------------------
+    |
+    | This option determines where the generated seed files will be saved.
+    |
+    */
+
+    'output_path' => database_path('seeds'),
+];
